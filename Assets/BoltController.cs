8b17811b0401a55dc68d57a34c082ce3a4a966e2
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2025-09-15 09:04:37 +0200

    Made Gustavs computer not explode. Closes #1

--- Assets/BoltController.cs
+++ Assets/BoltController.cs
@@ -5,11 +5,6 @@ public class BoltController : MonoBehaviour
   [SerializeField]
   float speed = 5f;
 
-  // void Start()
-  // {
-  //   Destroy(this.gameObject, 3);
-  // }
-
   void Update()
   {
     transform.Translate(Vector2.up * speed * Time.deltaTime);
